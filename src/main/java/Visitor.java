60a7e935b8b5456ec6186c1a83ce0c36b0d7d7b0
Chris Craig <ctc2@live.co.uk>
2018-07-02 20:58:39 +0100

    set up visitor

---
+++ src/main/java/Visitor.java
@@ -0,0 +1,15 @@
+public class Visitor {
+    private int age;
+    private float height;
+    private float money;
+
+
+    public int age(){
+        this.age = age;
+        this.height = height;
+        this.money = money;
+    }
+
+
+
+}
