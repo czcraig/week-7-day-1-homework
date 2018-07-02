ec74513e119be98936bfec2080f03abd57481b5b
Chris Craig <ctc2@live.co.uk>
2018-07-02 21:49:57 +0100

    created superclass Attraction

---
+++ src/main/java/Attraction.java
@@ -0,0 +1,15 @@
+public abstract class Attraction {
+
+    private String name;
+
+    public Attraction(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+
+
+}
